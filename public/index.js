3fdb3bdf5b759997e81e2ac45eaa7249ad117772
templeGo <harrypoppopoo@gmail.com>
2020-02-04 03:13:09 +0900

    エフェクトの追加

--- public/index.js
+++ public/index.js
@@ -8,6 +8,12 @@ $(document).on('click','#aboutnav', function()
     showAboutView();
 });
 
+$(document).on('click','.item img', function()
+{
+    var par = $(this).parent();
+    window.location.href = $('p', par).text();
+});
+
 function showAboutView()
 {
     $("#about").show();
